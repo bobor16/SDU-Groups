edf94a5b354df862781762060d2f3bb319c85ad7
Borgar Bordoy <bobor16@student.sdu.dk>
2019-10-05 17:01:09 +0200

    test

---
+++ SDU-Groups/SDU-Groups/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDU-Groups
+//
+//  Created by Simone Christensen on 05/10/2019.
+//  Copyright © 2019 Borgar Bordoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
