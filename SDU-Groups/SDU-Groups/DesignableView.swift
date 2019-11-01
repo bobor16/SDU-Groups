233d28a5b91678cb5a46c32b1717e05b3d967039
Martin Sørensen <mart.se.sorensen@gmail.com>
2019-11-01 14:58:31 +0100

    LoginScreen

---
+++ SDU-Groups/SDU-Groups/DesignableView.swift
@@ -0,0 +1,43 @@
+//
+//  DesignableView.swift
+//  SDU-Groups
+//
+//  Created by Martin Sebastian Sørensen on 01/11/2019.
+//  Copyright © 2019 Borgar Bordoy. All rights reserved.
+//
+
+//
+//  DesignableView.swift
+//  LoginExample
+//
+//  Created by Gary Tokman on 3/10/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
---
+++ SDU-Groups/SDU-Groups/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  SDU-Groups
+//
+//  Created by Martin Sebastian Sørensen on 01/11/2019.
+//  Copyright © 2019 Borgar Bordoy. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
