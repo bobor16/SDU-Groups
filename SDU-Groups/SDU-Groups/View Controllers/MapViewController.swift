97f8188697e882bf5a3427fe3c373fa99c488f57
Borgar Bordoy <bobor16@student.sdu.dk>
2019-11-02 12:27:01 +0100

    Added user location, eventual SDU latitude and longitude for app simulation

--- SDU-Groups/SDU-Groups/View Controllers/MapViewController.swift
+++ SDU-Groups/SDU-Groups/View Controllers/MapViewController.swift
@@ -8,61 +8,108 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController , CLLocationManagerDelegate {
+class MapViewController: UIViewController  {
     
+    /*
+     SDU's location - rookie mistake, use comma
+     Latitude = 55,368963
+     Longitude = 10;428282
+     */
+    
+ 
     @IBOutlet weak var mapView: MKMapView!
-    let regionRadius: CLLocationDistance = 500
     let locationManager = CLLocationManager()
+    let regionInMeters: Double = 400
+    
+    // alert button for not enabled gps
+    //let gpsNotEnabled = UIAlertController(title: "Location not found", message: "To use this function go to settings and enable gps.", preferredStyle: UIAlertController.Style.alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationServices()
 
         // Do any additional setup after loading the view.
         
-        self.locationManager.requestAlwaysAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
         }
-        
-      //  let initialLocation = CLLocation(latitude: 55.369099, longitude: 10.428296)
-        
-     //   centerMapOnSDU(location: initialLocation)
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-//        print("locations = \(locValue.latitude) \(locValue.longitude)")
-//    }
-  
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locValue: CLLocationCoordinate2D = manager.location!.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-        let userLocation = locations.last
-        let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
-        self.mapView.setRegion(viewRegion, animated: true)
-    }
-    func centerMapOnSDU(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Checks whether or not if user has enabled location in mobile settings
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+            
+        } else {
+            print("User must enable gps in settings")
+            //self.present(gpsNotEnabled, animated: true, completion: nil)
+            
+            
+        }
     }
-    */
-
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        // If user allows use of location
+        case .authorizedWhenInUse:
+            // Do Map Stuff
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            print("ShowUserLocation is set to true")
+            break
+        //If user denies use of location
+        case .denied:
+            // Show alert instructing them how to turn on permissions
+            print("Denied")
+            break
+        case .notDetermined:
+            locationManager.requestAlwaysAuthorization()
+            print("Not determined")
+            break
+        case .restricted:
+            // Show in alert letting them know what's up
+            print("Restricted")
+            break
+        case .authorizedAlways:
+            print("Authorized always")
+            break
+        default:
+            print("Default")
+            break
+        }
+    }
 }
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else {
+            return
+        }
+        print("Location = \(locValue.latitude) \(locValue.longitude)")
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+}
+  
+
