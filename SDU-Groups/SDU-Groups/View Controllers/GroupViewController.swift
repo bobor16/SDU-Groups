e9bc46ed5fefcf78d5003b3722e1a0acd21074a9
Borgar Bordoy <bobor16@student.sdu.dk>
2019-11-26 18:01:23 +0100

    Group buttons fixed and added background image

--- SDU-Groups/SDU-Groups/View Controllers/GroupViewController.swift
+++ SDU-Groups/SDU-Groups/View Controllers/GroupViewController.swift
@@ -11,54 +11,21 @@ import UIKit
 
 class GroupViewController: UIViewController{
     
-    func openURL(urlSt: String!) {
-        if let url = URL(string:urlSt), !url.absoluteString.isEmpty {
+    @IBAction func Schedule(_ sender: UIButton) {
+        print("Hello world")
+    }
+    
+    @IBAction func BookRoom(_ sender: UIButton) {
+        guard let url = URL(string: "https://mitsdu.sdu.dk/booking/default.aspx") else {
+            return
+        }
+        if UIApplication.shared.canOpenURL(url){
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
-    func openURL2(url2: String) {
-        if let url = URL(string: url2) {
-            do {
-                let contents = try String(contentsOf: url)
-                print(contents)
-            } catch {
-                
-            }
-        } else {
-            print("Bad url")
-        }
-    }
-
-    
-    
-    @IBAction func bookRoomBtn(_ sender: UIButton) {
-        if let url = URL(string: "https://www.hackingwithswift.com") {
-            UIApplication.shared.open(url)
-        }
-    }
-    @IBAction func myBookings(_ sender: UIButton) {
-        print("GG")
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-    
     }
-    
-    
-    
 }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
